34a1b4aeb3fd6e8ed181be4e2acc217502367ac6
Aleksey Belyakov <Abel139@yandex.ru>
2022-12-20 20:06:14 +0300

    на вторую проверку

--- scripts/index.js
+++ scripts/index.js
@@ -3,31 +3,27 @@ let popup = document.querySelector ('.popup');
 let popupCloseIcon = document.querySelector ('.popup__close-icon');
 let profileInfoPersonal = document.querySelector ('.profile-info__personal');
 let profileInfoActivity = document.querySelector ('.profile-info__activity');
-let popupPersonal = document.querySelector ('.popup__personal');
-let popupActivity = document.querySelector ('.popup__activity');
-let popupSaveButton = document.querySelector ('.popup__save-button');
+let popupPersonal = document.querySelector ('.popup_information_personal');
+let popupActivity = document.querySelector ('.popup_information_activity');
 let popupContainer = document.querySelector ('.popup__container');
 
 function openPopup () {
   popup.classList.add ('popup_opened');
+  popupPersonal.value = profileInfoPersonal.textContent;
+  popupActivity.value = profileInfoActivity.textContent;
 }
-
-profileInfoEditButton.addEventListener('click', openPopup);
 
 function closePopup () {
   popup.classList.remove ('popup_opened');
 }
 
-popupCloseIcon.addEventListener ('click', closePopup);
-
-popupPersonal.value = profileInfoPersonal.textContent;
-popupActivity.value = profileInfoActivity.textContent;
-
 function handleFormSubmit (evt) {
   evt.preventDefault();
   profileInfoPersonal.textContent = popupPersonal.value;
   profileInfoActivity.textContent = popupActivity.value;
+  closePopup();
 }
 
+profileInfoEditButton.addEventListener('click', openPopup);
 popupContainer.addEventListener('submit', handleFormSubmit);
-popupSaveButton.addEventListener('click', closePopup);
+popupCloseIcon.addEventListener ('click', closePopup);
